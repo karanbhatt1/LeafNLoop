e12f16a647258a6ca38496e73e3f47fea6b7975b
karanbhatt1 <karanbhatt759@gmail.com>
2025-04-09 23:18:41 +0530

    establised mysql connection with nodejs

--- sqlConnection.js
+++ sqlConnection.js
@@ -1,31 +1,12 @@
-import sql from "mysql2";
-import {v2 as cloudinary} from "cloudinary";
-
-cloudinary.config({
-    cloud_name: 'dpoy37daj', 
-    api_key: '558537842595179',
-    api_secret:'XsZTNVz4qGzww6kZqH7X9LrZZR0'
-});
-const db = sql.createConnection({
-  host: 'localhost',  
-  user: 'root',
-  password: 'Project2@24',
-  database: ''
+import sql from "mysql2/promise";
+import env from "dotenv";
+env.config();
+const db = sql;
+const pool = db.createPool({
+  host: process.env.MYSQL_HOST ,
+  user:  process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
 
-db.connect((err)=>{
-    if(err){
-        console.log(err.message);
-    }
-    console.log("connected to database");
-})
-cloudinary.api.resources(
-    { type:'upload', prefix: 'Home/products/' },  // Replace with your folder name
-    (error, result) => {
-      if (error) return console.error(error);
-      console.log(result.resources.map(img => img.public_id));
-    const urls = result.resources.map(img => img.secure_url);
-    console.log(urls);
-    });
-  
-      
+module.exports=pool;
